24f8972d51002af20755f23d9fc90fd09086f95a
unknown <victor.manduca@hotmail.com>
2026-05-05 06:57:09 -0300

    feat: add variables and mutability

---
+++ variableAndMutability/src/main.rs
@@ -0,0 +1,15 @@
+/**
+ * variables and mutability
+ *
+ */
+
+fn main() {
+    variables();
+}
+
+fn variables() {
+    let mut a = 5;
+    println!("The value of a is: {}", a);
+    a = 10;
+    println!("The value of a is: {}", a);
+}
