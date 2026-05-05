3bbb4de03f50f405377967e3af138a1bb0b780e9
unknown <victor.manduca@hotmail.com>
2026-05-05 07:03:55 -0300

    feat: add constants

---
+++ constants/src/main.rs
@@ -0,0 +1,15 @@
+/**
+ * constants
+ */
+
+ const PI: f32 = 3.1415;
+
+fn main() {
+    constants();
+}
+
+fn constants() {
+    const Y: i32 = 10;
+    println!("The value of Y is: {}", Y);
+    println!("The value of PI is: {}", PI);
+}
