383f9222fdf06e93f0cdd10d4651dc3529d639b7
unknown <victor.manduca@hotmail.com>
2026-05-11 18:36:41 -0300

    feat: add enums and improve readme

--- README.md
+++ README.md
@@ -233,3 +233,46 @@ struct BankAccount {
     balance: f64,
 }
 ```
+
+## Enums
+
+Example of simple enum:
+```rust
+enum IpAddrKind {
+    V4,
+    V6,
+}
+```
+
+Enum being used in a struct:
+```rust
+struct IpAddr {
+    kind: IpAddrKind,
+    address: String,
+};
+
+let home = IpAddr {
+    kind: IpAddrKind::V4,
+    address: String::from("127.0.0.1"),
+};
+```
+
+Enums can have datatype with their values too:
+```rust
+enum IpAddrKind {
+    V4(String),
+    V6(String),
+};
+
+let home = IpAddrKind::V4(String::from("127.0.0.1"));
+```
+
+And they can be *enhanced* as well:
+```rust
+enum IpAddrKind {
+    V4(u8, u8, u8, u8),
+    V6(String),
+};
+
+let home = IpAddrKind::V4(127, 0, 0, 1);
+```
--- enums/src/main.rs
+++ enums/src/main.rs
@@ -1,3 +1,40 @@
 fn main() {
-    println!("Hello, world!");
+    enum IpAddrKind {
+        V4,
+        V6,
+    }
+
+    let four = IpAddrKind::V4;
+    let six = IpAddrKind::V6;
+
+    fn route(ip_kind: IpAddrKind) {}
+
+    route(four);
+    route(IpAddrKind::V4);
+
+    // structs
+    struct IpAddr {
+        kind: IpAddrKind,
+        address: String,
+    };
+
+    let home = IpAddr {
+        kind: IpAddrKind::V4,
+        address: String::from("127.0.0.1"),
+    };
+
+    enum IpAddrKind2 {
+        V4(String),
+        V6(String),
+    };
+
+    let home2 = IpAddrKind2::V4(String::from("127.0.0.1"));
+
+    // enhanced enums
+    enum IpAddrKind3 {
+        V4(u8, u8, u8, u8),
+        V6(String),
+    };
+
+    let home3 = IpAddrKind3::V4(127, 0, 0, 1);
 }
